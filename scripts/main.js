2afea8c6f87edc5c830fa9272905f70e41088030
Matt <mattrpittman@gmail.com>
2020-08-13 19:24:44 -0600

    added image to roll

--- scripts/main.js
+++ scripts/main.js
@@ -12,12 +12,19 @@ window.onload =()=> {
     const output = document.getElementById("output");
     const allRolls = document.getElementById("total");
     const clear = document.getElementById("clear").addEventListener("click", reset);
-
+    const diceImgs = ["./images/dice/dice1.png",
+                      "./images/dice/dice2.png",
+                      "./images/dice/dice3.png",
+                      "./images/dice/dice4.png",
+                      "./images/dice/dice5.png",
+                      "./images/dice/dice6.png",
+                    ];
     var rolls = [];
 
     function dice(e) {
         const rand1 = Math.floor(Math.random() * 6) + 1;
-        output.innerHTML = rand1;
+        const dice = rand1-1;
+        output.setAttribute("href", diceImgs[dice]);
         rolls.push(rand1);
         // totalRolls.innerHTML = rolls.length;
         allRolls.innerHTML = rolls.join(", ");
